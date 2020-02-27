b9d55b97e292f3cf8313c0d2c345ab896ed25c20
slonikmak <slonikmak@gmail.com>
2020-02-27 14:15:38 +0300

    remove comments

--- src/main/java/org/my_company/postal_service/PackageUtils.java
+++ src/main/java/org/my_company/postal_service/PackageUtils.java
@@ -2,11 +2,6 @@ package org.my_company.postal_service;
 
 public class PackageUtils {
 
-    /**
-     * @param testingPackage package to test
-     * @param box box to test
-     * @return does the package fit in the box
-     */
     public boolean isPackageFitInBox(Package testingPackage, Box box){
         return box.getInnerWidth() >= testingPackage.getWidth()
                 && box.getInnerHeight() >= testingPackage.getHeight()
